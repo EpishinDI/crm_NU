54765494273f022796421fe44e51606d6e09ca34
EpishinDI <35424103+EpishinDI@users.noreply.github.com>
2019-10-09 12:31:22 +0300

    add form

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let app = express();
 let mysql = require('mysql');
 
 
+app.use(express.json());
 app.use(express.static("public"));
 
 app.set('view engine', 'pug');
@@ -18,21 +19,56 @@ app.listen(3000,()=>{
 	console.log('node start at 3000');
 });
 
+app.get('/new-client', (req, res) => {
+	res.render('new-client');
+});
+app.get('/', (req, res) => {
+	res.render('main');
+});
 
-app.get('/3', (req, res) => {
+app.get('/client-list', (req, res) => {
 	
 	con.query(
-		'SELECT * FROM users',
+		'SELECT * FROM clientele',
 		(error,result)=>{
 		if(error) throw error;
-		let usersArr = {};	
+		let clienteleArr = {};	
 		for(let i=0;i<result.length;i++){
-			usersArr[result[i]['id']] = result[i];
+			clienteleArr[result[i]['id']] = result[i];
 		}		
-		res.render('test',{
-			users: JSON.parse(JSON.stringify(usersArr))
+		res.render('client-list',{
+			clientele: JSON.parse(JSON.stringify(clienteleArr))
 		});
 		}
 	)
 });
+
+
+app.post('/send-form', (req, res) => {
+	saveOrder(req.body);
+});
+
+function saveOrder(data) {
+	let sql;
+	let maxId = 0;
+	con.query(
+		'SELECT * FROM clientele',
+		(error, result) => {
+			if (error) throw error;
+			for (let i = 0; i < result.length; i++) {
+				if(result[i]['id']>maxId) maxId = result[i]['id']
+			}
+		}
+	)
+
+
+	sql = "INSERT INTO clientele (id, lastName, firstName, middName, email, phone) VALUES ('" + maxId + "', '" + data.lastName + "', '" + data.firstName + "', '" + data.middleName + "','" + data.clientEmail + "','" + data.clientPhone + "')";
+	console.log(sql);
+	con.query(sql, function (error, result) {
+		if (error) throw error;
+		console.log("1 record inserted");
+	});
+}
+
+
 ///
---
+++ public/js/maine.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+	let btnPreliminaryForm = document.querySelector('.btnPreliminaryForm');
+	
+	let data = ['Волков', 'Николай', 'Сергеевич', 'wolf@mail.ru', '+79670802112', 'Табуретка', 'Должна быть очень удобная'];
+
+	for (let i = 0; i < btnPreliminaryForm.parentNode.length - 1; i++) {
+
+		btnPreliminaryForm.parentNode[i].value = data[i];
+
+}
+
+
+function sendForm(send) {
+	fetch('/send-form', {
+			method: 'POST',
+			body: JSON.stringify(send),
+			headers: {
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			}
+		})
+		.then(function (res) {
+
+		})
+		.then(function (body) {
+			console.log(body);
+		})
+}
+
+
+btnPreliminaryForm.addEventListener('click', () => {
+	let formData = {};
+	for (let i = 0; i < btnPreliminaryForm.parentNode.length - 1; i++) {
+
+		formData[btnPreliminaryForm.parentNode[i].id] = btnPreliminaryForm.parentNode[i].value
+	}
+
+	sendForm(formData);
+})
+}
